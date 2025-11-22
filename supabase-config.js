03a701563c0c08d6ae749978dd3cb08d530a8ed0
munjed80 <rain-man1980@hotmail.com>
2025-11-22 19:56:20 +0000

    Update Supabase anon key in configuration

--- supabase-config.js
+++ supabase-config.js
@@ -4,5 +4,6 @@
 window.SUPABASE_URL = 'https://klevfdkwixblkiqllrpq.supabase.co';
 
 // ضع الـ anon public key من تبويب API Keys (ليس service_role)
-window.SUPABASE_ANON_KEY = 'sb_publishable_WCdwnitNif5BNnXJZW7gPQ_aKG75-Ia';
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtsZXZmZGt3aXhibGtpcWxscnBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM4MzMyMTEsImV4cCI6MjA3OTQwOTIxMX0.vNBVa5h3AXShrmb4XmUmE7FKFtXiCwGefSMlW_Kxvmg';
+
 
